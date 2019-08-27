fed8594b06c44b56c1fc66bad5f6f7ece079e1d7
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2019-08-26 21:15:49 -0400

    add minor changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,10 +5,6 @@ const Navbar = () => {
   return (
     <div className="react-router">
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="/">
-          <img src="/docs/4.3/assets/brand/bootstrap-solid.svg" width="30" height="30" className="d-inline-block align-top" alt=""/>
-          Bootstrap
-        </a>
         <ul className="navbar-nav">
           <li className="nav-item">
             <NavLink to="/home" className="nav-link">Home <span className="sr-only">(current)</span></NavLink>
